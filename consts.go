a7b2ba281318c89dd3465526d00de5558c7e593e
toku463ne <toku46_3_ne@hotmail.com>
2021-07-29 19:14:14 +0900

    Removed non used consts

--- consts.go
+++ consts.go
@@ -1,14 +1,10 @@
 package csvdb
 
 const (
-	cRModePlain         = "plain"
-	cRModeGZip          = "gzip"
-	cMaxPartitionDitigs = 10
-	cTblIniExt          = "tbl.ini"
-	cDefaultPartitionID = "default"
-	cDefaultBuffSize    = 10000
-	CWriteModeAppend    = "a"
-	CWriteModeWrite     = "w"
-	cTableTypeCsv       = "csv"
-	cTableTypeRotating  = "rot"
+	cRModePlain      = "plain"
+	cRModeGZip       = "gzip"
+	cTblIniExt       = "tbl.ini"
+	cDefaultBuffSize = 10000
+	CWriteModeAppend = "a"
+	CWriteModeWrite  = "w"
 )
